b09d88b9f8d454017ffe4da80b4823d96b94c70b
Arillaxe <bekirshabut66@gmail.com>
2021-01-09 16:56:04 +0300

    Made phone number clickable

--- src/Components/ContactPhone/ContactPhone.jsx
+++ src/Components/ContactPhone/ContactPhone.jsx
@@ -4,7 +4,9 @@ const ContactPhone = () => {
   return (
     <div className="contactPhone">
       <div className="contactPhone-time">Ежедневно с 9:00 до 20:00</div>
-      <div className="contactPhone-phone">+7 (978) 585 38 15</div>
+      <div className="contactPhone-phone">
+        <a href="tel: +7 (978) 585 38 15">+7 (978) 585 38 15</a>
+      </div>
     </div>
   );
 };
